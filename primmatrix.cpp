9cf97c33e026b1884257324fd9ca1cc56c8f154c
ghfjkdls <61516241+ghfjkdls@users.noreply.github.com>
2020-04-29 23:38:18 +0200

    Add files via upload

---
+++ primmatrix.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "lista.h"
+#include "graflista.h"
+#include "grafmacierz.h"
+#include <fstream>
+#include "edgelist.h"
+
+
+
+int main(){
+	double start, stop, time;
+	std::ifstream file;
+
+
+	for(int i=0;i<100;i++){
+		grafmacierz *graf=new grafmacierz;
+		matlist *list=new matlist;
+		graf->fileread(file, i);
+		graf->primmatrixmake();
+		start=clock();
+		graf->prim();
+		stop=clock();
+		time= double(stop-start)/double(CLOCKS_PER_SEC);
+		std::cout<<time<<std::endl;
+		delete graf;
+		delete list;
+	}
+
+	return 0;
+}
